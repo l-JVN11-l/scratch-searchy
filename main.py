9a6a7b1332b263aa42fd54b8619fa5cbe8890a8d
Jivin Mazumder <105614440+l-JVN11-l@users.noreply.github.com>
2022-06-21 20:55:23 +0000

    Day of Dev #2

--- main.py
+++ main.py
@@ -3,26 +3,40 @@ from scratchattach import Encoding
 import os
 from better_profanity import profanity
 
-session = scratch3.login("STCSchoolAccount", os.environ['PASSWORD'])
+print(Encoding.decode('11834737333321'))
 
-conn = session.connect_cloud(project_id=os.environ['PID'])
+session = scratch3.Session("randomId", username="username")
 
-events = scratch3.CloudEvents(os.environ['PID'])
+
+
+conn = session.connect_cloud(project_id=679684393)
+
+events = scratch3.CloudEvents(679684393)
 
 @events.event
 def on_set(event): 
     if event.var == 'TO_HOST':
+      print(event.value)
+      
       type = Encoding.decode(event.value).split('-')
       if type[0] == "1":
         print ("Create Requested")
         print (type[1])
         check = type[1]      
-        bad_word = profanity.contains_profanity(check)     # 
-        if bad_word == "True":
-          print (bad_word)
+        bad_word = profanity.contains_profanity(check)
+        if bad_word == True:
+          to_project = Encoding.encode('Bad word detected!')
+
+          with open('bw-log.txt', 'a') as bwlogs:
+            bwlogs.write(f'\n"{event.value}" written by {event.user} >:(')
+        
+          conn.set_var("FROM_HOST", to_project)
         else:
-          print (bad_word)
-          
+          with open('database.txt', 'a') as file:
+            file.write(f"\n{type[1]}")
+
+            conn.set_var("FROM_HOST",Encoding.encode("Added, If anything is false a Searchy mod will change it")())
+          #make it add type[1] to database.txt
 
 
         
@@ -30,20 +44,22 @@ def on_set(event):
         print ("Get data")
         print (type[1])  
         f = open('database.txt',  "r")
-        found_data = 0
-        for x in f:
+        found_data = "a"
+        for x in f:         
           if type[1] in x:
-            found_data = 1
+            print("this statement works")
+            found_data = "b"
             info = x
-
+         
         print (found_data)
-        if found_data == 0:
+        if found_data == "a":
           to_project = Encoding.encode('FROM_HOST : ERROR 404')
+          print("Error 404")
+          conn.set_var("FROM_HOST", to_project)
 
-        elif found_data == 1:
-          to_project = Encoding.encode(info) 
-        
-        conn.set_var("FROM_HOST", to_project)
+        elif found_data == "b":
+          print("Found")
+          conn.set_var("FROM_HOST", Encoding.encode(info))
           
 @events.event 
 def on_ready():
